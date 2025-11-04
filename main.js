5d8dbe3727549ad82a9cc6431e194859cd513513
⊹𝐉𝐨𝐧𝐬 ↩︎ <jonathannovais21@gmail.com>
2025-11-04 16:54:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-const resposta = `https://api.disneyapi.dev/character/4`;
+const resposta = `https://api.disneyapi.dev/character/43`;
 
-async function chamar() {
+async function chamar()  {
     const resp = await fetch(resposta);
+
 
     const obj = await resp.json();
     const person = obj.data;
     document.getElementById(
         "personagem"
-    ).innerHTML = `<img src="${person.imageUrl}"
-    alt="${person.name}">`;
+    ).innerHTML = `<img src="${person.imageUrl}">`;
     document.getElementById(
         "nome"
-    ).innerHTML = 'O nome do personagem: ${person,name}';
+    ).innerHTML = `O nome do personagem: ${person.name}`;
+
 }
 
 chamar();
